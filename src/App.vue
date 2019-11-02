a5e346d6ef407bd9885320c4ef309294d0b7017e
Tin Huynh <tinhuynh@bocasay.com>
2019-11-02 20:08:15 +0700

    Refactor code base

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <ms-header/>
+    <div class="main-wrapper">
+      <router-view/>
     </div>
-    <router-view/>
+    <ms-footer/>
   </div>
 </template>
+
+<script>
+import msHeader from '@/components/header/ms-header'
+import msFooter from '@/components/footer/ms-footer'
+
+export default {
+  components: {
+    msHeader,
+    msFooter
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
